24dce93f00f053a8c279037ad762c84e0aeadd36
ATX <thahn1230@g.skku.edu>
2023-12-29 07:58:07 +0900

    finished backend work

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, HTTPException, UploadFile, File
+from fastapi import FastAPI, HTTPException, UploadFile, File, Form
 from app.middleware_config import add_middleware
-from .models.models import ModelRequest, ModelResponse, TrashType, TTSRequest, TTSResponse
+from .models.models import ModelResponse, TrashType, TTSResponse
 import json
 
 from services.classify_picture import classify_image
@@ -16,7 +16,7 @@ add_middleware(app)
 def root():
     return {"status": 200, "message": "hello from server"}
 
-
+# 학습된 모델에게 사진 주고 어떤 종류의 쓰레기인지 예측하게 하기
 @app.post("/get_response_from_model", response_model=ModelResponse)
 async def get_response_from_model(file: UploadFile = File(...)):
     img_np = await img_to_np_array(file)
@@ -28,8 +28,21 @@ async def get_response_from_model(file: UploadFile = File(...)):
     print(predicted_trash_type)
     return ModelResponse(answer=predicted_trash_type)
 
+# tts로 읽어야 하는 쿼리문을 frontend로 전송
 @app.post("/get_tts_query", response_model=TTSResponse)
-async def get_tts_query(item: TTSRequest):
-    with open("./data/recycle_time.json", 'r') as file:
+async def get_tts_query(name: str = Form(...)):
+    print(name)
+    # 미리 json으로 저장해놓은 tts 쿼리문을 파일로 열기
+    with open("./data/recycle_time.json", 'r', encoding='utf-8') as file:
         data_json = json.load(file)
-    return data_json[item.query]
+        
+    # 전달받은 name 인자와 같은 json의 query 데이터를 반환
+    for entry in data_json:
+        if entry["name"] == name:
+            query = entry["query"]
+            print(query)
+            return TTSResponse(query=query)
+        
+    # 실패시 쿼리문이 존재하지 않는다는 것을 명시
+    print(f"No matching name found for: {name}")
+    return TTSResponse(query="No query found for the given name.")
--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from enum import Enum
 
-
+# 쓰레기 분류
 class TrashType(str, Enum):
     일반쓰레기 = "일반쓰레기"
     플라스틱 = "플라스틱"
@@ -10,15 +10,18 @@ class TrashType(str, Enum):
     유리 = "유리"
     종이 = "종이"
 
-
+# model이 받아야 하는 데이터 타입 선언
 class ModelRequest(BaseModel):
     image: str
 
+# model이 반환해야 하는 데이터 타입 선언
 class ModelResponse(BaseModel):
     answer: TrashType
 
+# tts 쿼리를 위해 받아야 하는 데이터 타입 선언
 class TTSRequest(BaseModel):
     name: str
 
+# tts 쿼리가 반환해야 하는 데이터 타입 선언
 class TTSResponse(BaseModel):
     query: str
